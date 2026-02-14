0c3896aea4a481b4f7d6f8ce2e4f9e979fc14f9c
ghifar1327 <givartorreto@gmail.com>
2026-02-14 22:04:55 +0700

    feat: add feature for render list menu

---
+++ internal/menu/handler.go
@@ -0,0 +1,14 @@
+	package menu
+
+	import "fmt"
+
+	func ShowMenu(){
+		menus , err := FecthMenu()
+		if err != nil {
+			fmt.Println("Error:" , err)
+		}
+
+		for  i, m := range menus{
+			fmt.Printf("%d. %s, harga: Rp.%d\n",i+1, m.Name, m.Price )
+		}
+	}
