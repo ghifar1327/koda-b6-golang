a42315b3a08f38917a02420252eae5c009b981e7
ghifar1327 <givartorreto@gmail.com>
2026-02-15 00:49:34 +0700

    feat: adds function ShowMenu for render list menu

--- internal/menu/handler.go
+++ internal/menu/handler.go
@@ -1,14 +1,16 @@
-	package menu
+package menu
 
-	import "fmt"
+import (
+	"fmt"
+)
 
-	func ShowMenu(){
-		menus , err := FecthMenu()
-		if err != nil {
-			fmt.Println("Error:" , err)
-		}
-
-		for  i, m := range menus{
-			fmt.Printf("%d. %s, harga: Rp.%d\n",i+1, m.Name, m.Price )
-		}
+func ShowMenu() {
+	menus, err := FecthMenu()
+	if err != nil {
+		fmt.Println("Error:", err)
 	}
+
+	for i, m := range menus {
+		fmt.Printf("%d. %s, harga: Rp.%d\n", i+1, m.Name, m.Price)
+	}
+}
