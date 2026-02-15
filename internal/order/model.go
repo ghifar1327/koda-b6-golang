a7941d6cfd6993c03f8f7ec03a1e96b0375cd829
ghifar1327 <givartorreto@gmail.com>
2026-02-15 23:30:58 +0700

    feat: create a calc method to calculate the subtotal price of items with an interface contract

--- internal/order/model.go
+++ internal/order/model.go
@@ -14,6 +14,25 @@ type OrderItem struct {
 type Transaction struct {
 	Id        int
 	Items     []OrderItem
-	Total     int
+	GrandTotal     int
 	CreatedAt time.Time
+}
+
+func (o *OrderItem) Calc() int {
+	o.Subtotal = o.Price * o.Qty
+	return o.Subtotal
+}
+
+func (t *Transaction) Calc() int {
+	var total int
+	for i := range t.Items {
+		total += t.Items[i].Calc()
+	}
+	t.GrandTotal = total
+	return t.GrandTotal
+}
+
+
+type Calculate interface{
+	Calc() int
 }
