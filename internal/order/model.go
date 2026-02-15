b49fc62ed904848ba6e3a6864baa80456b6c01dc
ghifar1327 <givartorreto@gmail.com>
2026-02-15 14:17:44 +0700

    feat: add struck transaction for check out feature

--- internal/order/model.go
+++ internal/order/model.go
@@ -1,9 +1,19 @@
 package order
 
-type Item struct {
+import "time"
+
+type OrderItem struct {
 	Id       int
 	Name     string
 	Price    int
 	Qty      int
 	Subtotal int
+}
+
+
+type Transaction struct {
+	Id        int
+	Items     []OrderItem
+	Total     int
+	CreatedAt time.Time
 }
